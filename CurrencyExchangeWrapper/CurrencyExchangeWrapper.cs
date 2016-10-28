1f4bfb2d2dc27a613426c53b3eee588713dbc90a
Andreas Reschenhofer <andreas-reschenhofer@gmx.at>
2016-10-28 11:48:57 +0200

    First Commit

---
+++ CurrencyExchangeWrapper/CurrencyExchangeWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchangeWrapper
+{
+    public class CurrencyExchangeWrapper
+    {
+        [DllImport("CurrencyExchange.dll")]
+        public static extern double currencyExchange_exchange(double amount, int from, int to);
+        [DllImport("CurrencyExchange.dll")]
+        public static extern void currencyExchange_info();
+        [DllImport("CurrencyExchange.dll")]
+        public static extern void currencyExchange_printVersion();
+    }
+}
---
+++ SST_LB02/MainApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyExchangeWrapper;
+
+namespace SST_LB02
+{
+    class MainApplication
+    {
+        static void Main(string[] args)
+        {
+            CurrencyExchangeWrapper.CurrencyExchangeWrapper.currencyExchange_printVersion();
+            CurrencyExchangeWrapper.CurrencyExchangeWrapper.currencyExchange_info();
+            double test = CurrencyExchangeWrapper.CurrencyExchangeWrapper.currencyExchange_exchange(45.12, 0, 1);
+        }
+    }
+}
